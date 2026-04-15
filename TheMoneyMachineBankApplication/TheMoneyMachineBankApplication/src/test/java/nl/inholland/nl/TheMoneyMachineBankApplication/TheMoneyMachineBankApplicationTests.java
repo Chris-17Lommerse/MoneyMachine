849be52250e719898d55bde0392efb4a79ff3bfd
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-04-15 20:15:50 +0200

    Spring Boot Bank Application setup
    Spring Boot Project Aangemaakt

---
+++ TheMoneyMachineBankApplication/TheMoneyMachineBankApplication/src/test/java/nl/inholland/nl/TheMoneyMachineBankApplication/TheMoneyMachineBankApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.nl.TheMoneyMachineBankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMoneyMachineBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
