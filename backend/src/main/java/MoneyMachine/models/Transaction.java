3b17bc0408b410d9779d81a3cfe2371973c964e4
WesselB1234 <stosin007@hotmail.com>
2026-05-06 13:50:51 +0200

    Feat: added not nullable annotations to transactions

--- backend/src/main/java/MoneyMachine/models/Transaction.java
+++ backend/src/main/java/MoneyMachine/models/Transaction.java
@@ -2,11 +2,13 @@ package MoneyMachine.models;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,10 +26,18 @@ public class Transaction {
     @Id
     @GeneratedValue
     private long transactionId;
+
     @ManyToOne
+    @JoinColumn(nullable = false)
     private User initiatingUser;
+
+    @Column(nullable = false)
     private BigDecimal amount;
+
+    @Column(nullable = false)
     private String message;
+
+    @Column(nullable = false)
     private Boolean isActive;
 
     public Transaction(User initiatingUser, BigDecimal amount, String message, Boolean isActive) {
