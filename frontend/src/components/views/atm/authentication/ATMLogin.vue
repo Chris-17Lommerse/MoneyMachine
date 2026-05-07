2cc19fe6820fb8afac2a36494702e66ce7b0d6ae
WesselB1234 <stosin007@hotmail.com>
2026-05-07 19:57:14 +0200

    Feat: added atm login vue components

---
+++ frontend/src/components/views/atm/authentication/ATMLogin.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import ATMLoginForm from '../../../organisms/forms/authentication/ATMLoginForm.vue';
+</script>
+
+<template>
+    <section class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card shadow-sm authentication-box">
+            <div class="card-body">
+                <h3 class="card-title text-center mb-4">Login</h3>
+                <ATMLoginForm />
+            </div>
+        </div>
+    </section>
+</template>
--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,8 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from "../components/HelloWorld.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+import ATMLogin from '@/components/views/atm/authentication/ATMLogin.vue'
+import AdminAuthorizationTest from '@/components/views/atm/authentication/AdminAuthorizationTest.vue'
+import UserAuthorizationTest from '@/components/views/atm/authentication/UserAuthorizationTest.vue'
+import AtmLayout from '@/components/layout/AtmLayout.vue'
 
 const routes = [
-    {path: '/', component: HelloWorld}
+    {
+        path: '/', 
+        component: HelloWorld
+    },
+    {
+        path: '/atm',
+        component: AtmLayout,
+        children: [
+            { 
+                path: 'login', 
+                component: ATMLogin, 
+                meta: { 
+                    title: 'Login'
+                }
+            },
+            { 
+                path: 'admin-test', 
+                component: AdminAuthorizationTest,
+                meta: { 
+                    title: 'AdminTest',
+                    isAuthenticated: true,
+                    roles: ['Admin']
+                }
+            },
+            { 
+                path: 'user-test', 
+                component: UserAuthorizationTest,
+                meta: { 
+                    title: 'UserTest',
+                    isAuthenticated: true
+                }
+            },
+        ],
+    },
 ]
 
 const router = createRouter({
