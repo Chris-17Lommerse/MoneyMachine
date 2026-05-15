6adb12b1404f9f0185ab00dd95f787e38a6cd954
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-05-15 13:10:55 +0200

    Begonnen met refactore van de code.

--- backend/src/main/java/MoneyMachine/services/UserServiceImpl.java
+++ backend/src/main/java/MoneyMachine/services/UserServiceImpl.java
@@ -1,33 +1,33 @@
 package MoneyMachine.services;
 
 import java.util.ArrayList;
-import MoneyMachine.mappers.UserMapper;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import MoneyMachine.mappers.UserMapper;
 import MoneyMachine.models.User;
 import MoneyMachine.models.dtos.UserDTO;
 import MoneyMachine.repositories.UserRepository;
 import MoneyMachine.services.interfaces.UserService;
-import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private UserMapper userMapper;
     private UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper)
     {
-        this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.userRepository = userRepository;
     }
 
     public List<UserDTO> getAllUsersWithoutBankAccounts() {
         Iterable<User> users = userRepository.findByBankAccountsIsEmpty();
         List<UserDTO> convertedUsers = new ArrayList<UserDTO>();
 
-        for(User user : users)
-        {
+        for (User user : users) {
             convertedUsers.add(userMapper.toDTO(user));
         }
         return convertedUsers;
