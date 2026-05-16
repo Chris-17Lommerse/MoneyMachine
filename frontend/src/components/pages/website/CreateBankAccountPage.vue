e0247dab4db7cce896ead2131b86adc3b714c38a
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-05-16 11:58:36 +0200

    Begin gemaakt met de frontend

---
+++ frontend/src/components/pages/website/CreateBankAccountPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from '@/utils/axios';
+
+const loading = ref(true);
+const error = ref(null);
+const userWithoutBankAccount = ref([]);
+
+</script>
+
+<template>
+
+</template>
