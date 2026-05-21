7b670cd6bfdaf37bfbfa0153b2ffa586ae312945
WesselB1234 <stosin007@hotmail.com>
2026-05-21 15:16:02 +0200

    Feat: made login/register requests to all requests even if jwt included

--- frontend/src/components/molecules/nav/NavMenu.vue
+++ frontend/src/components/molecules/nav/NavMenu.vue
@@ -10,8 +10,11 @@
 <template>
     <ul class="navbar-nav mr-auto">
         <template v-if="websiteDecodedAuthToken !== null">
-            <NavLink to="/user-test" text="Test your JWT" />
-            <NavLink to="/employee-test" text="Test your employee rights" />  
+            <NavLink to="/user-test" text="Test your JWT" /> 
+        </template>
+
+        <template v-if="websiteDecodedAuthToken.role === 'EMPLOYEE'">
+            <NavLink to="/employee-test" text="Test your employee rights" /> 
         </template>
 
         <NavLink to="/atm/login" text="Go to ATM" /> 
