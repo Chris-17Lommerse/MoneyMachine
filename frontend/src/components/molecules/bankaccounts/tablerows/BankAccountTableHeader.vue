6c6cedb156b7d8f6314366354a792bbedfe9fee3
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-05-21 19:18:56 +0200

    Kleine fix

--- frontend/src/components/molecules/bankaccounts/tablerows/BankAccountTableHeader.vue
+++ frontend/src/components/molecules/bankaccounts/tablerows/BankAccountTableHeader.vue
@@ -1,16 +1,9 @@
 <script setup>
-import ActionsTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/ActionsTableHeaderAtom.vue';
-import BSNNumberTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/BSNNumberTableHeaderAtom.vue';
-import EmailTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/EmailTableHeaderAtom.vue';
-import FirstNameTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/FirstNameTableHeaderAtom.vue';
-import LastNameTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/LastNameTableHeaderAtom.vue';
-import PhoneNumberTableHeaderAtom from '../../../atoms/users/textelements/tableheaders/PhoneNumberTableHeaderAtom.vue';
 import AbsoluteLimitTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/AbsoluteLimitTableHeaderAtom.vue';
 import BalanceTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/BalanceTableHeaderAtom.vue';
 import BankAccountTypeTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/BankAccountTypeTableHeaderAtom.vue';
 import DailyTransferLimitTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/DailyTransferLimitTableHeaderAtom.vue';
 import IBANNumberTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/IBANNumberTableHeaderAtom.vue';
-import SingleTransferLimitTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/SingleTransferLimitTableHeaderAtom.vue';
 import SingleTransferLimitTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/SingleTransferLimitTableHeaderAtom.vue';
 import IsActiveTableHeaderAtom from '../../../atoms/bankaccounts/textelements/tableaheaders/IsActiveTableHeaderAtom.vue';
 </script>
@@ -24,6 +17,5 @@ import IsActiveTableHeaderAtom from '../../../atoms/bankaccounts/textelements/ta
         <DailyTransferLimitTableHeaderAtom />
         <SingleTransferLimitTableHeaderAtom />
         <IsActiveTableHeaderAtom />
-        <ActionsTableHeaderAtom />
     </tr>
 </template>
