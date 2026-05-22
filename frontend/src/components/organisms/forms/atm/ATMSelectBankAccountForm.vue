8bf41c03c462f3c25143b5375a43e2671b6e334c
WesselB1234 <stosin007@hotmail.com>
2026-05-22 18:24:44 +0200

    Feat: display available bank accounts in bank account selection

--- frontend/src/components/organisms/forms/atm/ATMSelectBankAccountForm.vue
+++ frontend/src/components/organisms/forms/atm/ATMSelectBankAccountForm.vue
@@ -1,24 +1,56 @@
 <script setup>
-    import { ref } from 'vue'
+    import { onMounted, ref } from 'vue'
+    import { useErrorHandlingStore } from '@/stores/errorHandlingStore.js'
+    import { useAuthStore } from '@/stores/authStore.js'
+    import axios from '@/utils/axios.js'
+
     import FormLabel from '@/components/atoms/forms/FormLabel.vue'
     import SubmitBtn from '@/components/atoms/buttons/SubmitBtn.vue'
+    import ErrorAlert from '@/components/atoms/errorHandling/ErrorAlert.vue'
+    import SuccessAlert from '@/components/atoms/errorHandling/SuccessAlert.vue'
+
+    const errorHandlingStore = useErrorHandlingStore()
+    const authStore = useAuthStore()
 
     const selectedBankAccountIban = ref('')
+    const bankAccounts = ref(null)
 
-    async function handleLogin(e) {
-        e.preventDefault()
-        console.log(selectedBankAccountIban.value)
+    function handleSelectBankAccount(e) {
+        try {
+            e.preventDefault()
+            console.log(selectedBankAccountIban.value)
+        }
+        catch (ex){
+            errorHandlingStore.errorMessage = ex.message   
+        }
     }
+
+    onMounted(async () => {
+        try {
+            bankAccounts.value = (await axios.get('users/' + authStore.atmDecodedAuthToken.sub + '/bank-accounts')).data
+        }
+        catch (ex){
+            if (ex.response){
+                errorHandlingStore.errorMessage = ex.response.data.message
+            }
+            else {
+                errorHandlingStore.errorMessage = ex.message
+            }
+        }
+    })
 </script>
 
 <template>
-    <form @submit="handleLogin">
+
+    <SuccessAlert />
+    <ErrorAlert />
+
+    <form @submit="handleSelectBankAccount">
         <div class="mb-3">
             <FormLabel id="bankAccount" labelName="Select a bank account" />
             <select v-model="selectedBankAccountIban" id="bankAccount" class="custom-select">
                 <option value="" disabled>Select an account</option>
-                <option value="1">hi</option>
-                <option value="2">pootis</option>
+                <option v-for="bankAccount in bankAccounts" :value="bankAccount.iban">{{ bankAccount.iban }}</option>
             </select>
         </div>
 
