069d056fa17394f85b541ad422614e0d75bc6ab5
mydw333 <menno.dewever@gmail.com>
2026-05-28 14:03:58 +0200

    opslaan

--- backend/src/main/java/MoneyMachine/controllers/TransactionController.java
+++ backend/src/main/java/MoneyMachine/controllers/TransactionController.java
@@ -2,7 +2,6 @@ package MoneyMachine.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,20 +21,11 @@ public class TransactionController {
         return ResponseEntity.ok(transactionService.getAllTransactions());  
     }
 
-    @GetMapping("/transactions/account/{accountId}")
-    public ResponseEntity<?> getTransactionsByAccountiban(@PathVariable String Iban)
-    {
-        return ResponseEntity.ok(transactionService.getAllTransactionsByAccountId(Iban));
-    }
-
     @PostMapping("/transactions/transfer")
     public ResponseEntity<?> createTransfer(@RequestBody TransferRequest transaction) {
        
        return ResponseEntity.ok(transactionService.createTransfer(transaction));
     }
 
-    @GetMapping("/transactions/{id}")
-    public ResponseEntity<?> getTransactionById(@PathVariable Long id) { 
-        return ResponseEntity.ok(transactionService.getTransactionByid(id));
-    }
+   
 }
--- backend/src/main/java/MoneyMachine/repositories/BankAccountRepository.java
+++ backend/src/main/java/MoneyMachine/repositories/BankAccountRepository.java
@@ -2,16 +2,16 @@ package MoneyMachine.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import MoneyMachine.models.BankAccount;
 import jakarta.persistence.LockModeType;
 
 @Repository
-public interface BankAccountRepository extends ListCrudRepository<BankAccount, String> {
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT b FROM BankAccount b WHERE b.iban = :iban")
     Optional<BankAccount> findByIdForUpdate(String iban);
