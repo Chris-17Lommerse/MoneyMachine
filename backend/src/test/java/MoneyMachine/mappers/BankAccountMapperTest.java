deb46803827db4c2aa6b9b553ddb12c2705ec9b1
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-05-30 21:15:11 +0200

    Finished Mapper tests

--- backend/src/test/java/MoneyMachine/mappers/BankAccountMapperTest.java
+++ backend/src/test/java/MoneyMachine/mappers/BankAccountMapperTest.java
@@ -1,24 +1,30 @@
 package MoneyMachine.mappers;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import MoneyMachine.models.BankAccount;
 import MoneyMachine.models.User;
+import MoneyMachine.models.dtos.responses.BankAccountResponse;
 import MoneyMachine.models.enums.BankAccountType;
 public class BankAccountMapperTest {
     private List<BankAccount> bankAccounts;
     private BankAccount bankAccount;
     private User user;
+    private BankAccountMapper bankAccountMapper;
 
     @BeforeEach
     void setUp() {
+        bankAccountMapper = new BankAccountMapper();
         bankAccount = new BankAccount();
-        user = new User();
+        bankAccounts = new ArrayList<BankAccount>();
+;       user = new User();
+        user.setId(1L);
         bankAccount.setIban("NL91ABNA0417164300");
         bankAccount.setUser(user);
         bankAccount.setBalance(BigDecimal.valueOf(200));
@@ -32,19 +38,24 @@ public class BankAccountMapperTest {
     @Test
     void toResponseShouldNotBeNull()
     {
-        BankAccountMapper bankAccountMapper = new BankAccountMapper();
-        bankAccountMapper.toResponse(bankAccount);
-        assertNotNull(bankAccountMapper);
-        assertNotEquals(bankAccount.getIban(), bankAccountMapper.toResponse(bankAccount).getIban());
-        assertNotEquals(bankAccount.getUser().getId(), bankAccountMapper.toResponse(bankAccount).getUserId());
-        assertNotEquals(bankAccount.getAbsoluteLimit(), bankAccountMapper.toResponse(bankAccount).getAbsoluteLimit());
+        BankAccountResponse bankAccountResponse = bankAccountMapper.toResponse(bankAccount);
+        assertEquals(bankAccount.getIban(), bankAccountResponse.getIban());
+        assertEquals(user.getId(), bankAccountResponse.getUserId());
+        assertEquals(bankAccount.getAbsoluteLimit(), bankAccountResponse.getAbsoluteLimit());
+        assertEquals(bankAccount.getBalance(), bankAccountResponse.getBalance());
+        assertEquals(bankAccount.getBankAccountType(), bankAccountResponse.getBankAccountType());
+        assertEquals(bankAccount.getIsActive(), bankAccountResponse.isActive());
+        assertEquals(bankAccount.getSingleTransferLimit(), bankAccountResponse.getSingleTransferLimit());
+        assertEquals(bankAccount.getDailyTransferLimit(), bankAccountResponse.getDailyTransferLimit());
     }
 
     @Test
     void toResponseListShouldNotBeNull()
-    {
-        BankAccountMapper bankAccountMapper = new BankAccountMapper();
-        bankAccountMapper.toResponseList(bankAccounts);
-        assertNotEquals(bankAccountMapper.toResponseList(bankAccounts).size(), 0);
+    {   
+        bankAccounts.add(bankAccount);
+        List<BankAccountResponse> bankAccountResponses = bankAccountMapper.toResponseList(bankAccounts);
+        assertEquals(bankAccountResponses.size(), 1);
+        assertEquals(bankAccountResponses.getFirst().getIban(), bankAccount.getIban());
+        assertEquals(bankAccountResponses.getFirst().getUserId(), bankAccount.getUser().getId());
     }
 }
