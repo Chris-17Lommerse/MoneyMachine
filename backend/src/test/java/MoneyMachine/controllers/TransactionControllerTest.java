231cfbdb2a17acf2b3a62c9d3773a9d578573f55
WesselB1234 <stosin007@hotmail.com>
2026-06-02 22:47:18 +0200

    Feat: transaction test methods to fill

--- backend/src/test/java/MoneyMachine/controllers/TransactionControllerTest.java
+++ backend/src/test/java/MoneyMachine/controllers/TransactionControllerTest.java
@@ -11,12 +11,47 @@ public class TransactionControllerTest extends BaseControllerTest {
     }
 
     @Test
-    void testDeposit() {
+    void deposit_whenAuthorized_depositAmount() {
 
     }
 
     @Test
-    void testWithdraw() {
+    void depositOnOtherUser_whenAuthorized_depositAmount() {
+
+    }
+
+    @Test
+    void failDepositMaxLimit_whenDepositAboveMaxLimit_displayError() {
+
+    }
+
+    @Test
+    void failDepositAuthorize_whenDepositOtherUserNotOtherized_displayError() {
+
+    }
+
+    @Test
+    void withdraw_whenAuthorized_withdrawAmount() {
+
+    }
+
+    @Test
+    void withdrawFromOtherUser_whenAuthorized_withdrawAmount() {
+
+    }
+
+    @Test
+    void failWithdrawMaxLimit_whenWithdrawAboveMaxLimit_displayError() {
+
+    }
+
+    @Test
+    void failWithdrawAbsoluteLimit_whenWithdrawUnderAbsoluteLimit_displayError() {
+
+    }
+
+    @Test
+    void failWithdrawAuthorize_whenWithdrawOtherUserNotOtherized_displayError() {
 
     }
 }
