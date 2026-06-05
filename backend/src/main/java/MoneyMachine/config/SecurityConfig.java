222912f42e18a2808ee53ada8417505763c052ce
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-06-05 21:49:38 +0200

    Revert

--- backend/src/main/java/MoneyMachine/config/SecurityConfig.java
+++ backend/src/main/java/MoneyMachine/config/SecurityConfig.java
@@ -36,12 +36,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOriginPatterns(List.of(
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://chris-17lommerse.github.io",
-        "https://chris-17lommerse.github.io/*"
-    ));
+        configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
