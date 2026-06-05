f623c6821280ff4293d488e27791fa40c1accbe8
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-06-05 21:38:25 +0200

    Changed the CORS configuration

--- backend/src/main/java/MoneyMachine/config/SecurityConfig.java
+++ backend/src/main/java/MoneyMachine/config/SecurityConfig.java
@@ -36,7 +36,12 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(frontendUrl));
+            configuration.setAllowedOriginPatterns(List.of(
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://chris-17lommerse.github.io",
+        "https://chris-17lommerse.github.io/*"
+    ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
