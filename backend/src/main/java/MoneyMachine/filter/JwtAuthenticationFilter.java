f4eb5c1ec561598cfb470109ca085b10cf346fb9
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2026-06-05 23:08:58 +0200

    Smaal update

--- backend/src/main/java/MoneyMachine/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/MoneyMachine/filter/JwtAuthenticationFilter.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+
 import java.util.Optional;
 
 @Component
@@ -34,6 +35,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 
         try {
+            if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+                filterChain.doFilter(request, response);
+                return;
+            }
             String authorizationHeader = request.getHeader("Authorization");
 
             if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
