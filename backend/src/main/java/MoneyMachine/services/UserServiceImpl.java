5c80324fe6dd9abbd73cdcabad75c85cea99b3b0
mydw333 <menno.dewever@gmail.com>
2026-06-08 14:44:33 +0200

    bugfixes

--- backend/src/main/java/MoneyMachine/services/UserServiceImpl.java
+++ backend/src/main/java/MoneyMachine/services/UserServiceImpl.java
@@ -35,7 +35,7 @@ public class UserServiceImpl implements UserService {
     private TransactionService transactionService;
 
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper,
-            BankAccountService bankAccountService, TransactionService transactionService) {
+        BankAccountService bankAccountService, TransactionService transactionService) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
         this.bankAccountService = bankAccountService;
